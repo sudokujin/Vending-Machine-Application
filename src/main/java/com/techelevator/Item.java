fc63354887d0440b8322a3b1760635adf72a164d
Eric Roberson <fcs2014eroberson@gmail.com>
2023-02-09 09:26:35 -0700

    finished Item class

---
+++ src/main/java/com/techelevator/Item.java
@@ -0,0 +1,45 @@
+package com.techelevator;
+
+public abstract class Item {
+    private String name;
+    private double price;
+    private int inventory;
+
+
+    private String identifier;
+    private final int minCount = 0;
+    private final int maxCount = 5;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+   //abstract methods
+    public abstract String getSound();
+    
+}
